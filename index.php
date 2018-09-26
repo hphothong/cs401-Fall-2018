bf2ad13bb931d49b37549a17bed051a240462a45
Hayden Phothong <haydenphothong@u.boisestate.edu>
2018-09-25 23:04:13 -0600

    Adding styles for boxes, and changing color scheme

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include_once "header.php"; ?>
 <?php
-	for($i = 0; $i < 10; $i++)
+	for($i = 0; $i < 25; $i++)
 	{
-		echo '<div class="box">box</div>';
+		echo "<div class='box'>box {$i}</div>";
 	}
 ?>
 <?php include_once "footer.php"; ?>
